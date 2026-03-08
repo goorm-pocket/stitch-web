e246e9c7c60eb8bbfe1caa8723f4e63cc8265977
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-09 01:51:52 +0900

    ✨ feat(fe): body 배경색 설정 및 테마 icon 색상 추가
    본문 (무엇을, 왜 변경했는지 상세 설명)
    
    Resolves: #이슈번호

--- src/app/styles/globalStyle.ts
+++ src/app/styles/globalStyle.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing:border-box
     }
     body{
+        background-color:${({ theme }) => theme.colors.background};
         margin:0;
         padding:0;
     }
