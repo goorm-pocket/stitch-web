cd9e040525efd3330fc569ab30c4980a04eb95a5
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-08 22:41:32 +0900

    ✨ feat(fe): init Footer
    본문
    - Footer 파일 생성 및 레이아웃 등록
    
    Resolves: #이슈번호

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../../shared/components/Header";
+import Footer from "../../shared/components/Footer";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
