f8f3cfb7b47ec5b1d5bd38ddd31928e699cc4f76
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-11 18:16:06 +0900

    ✨ feat(fe): Bubble UI 구현
    본문
    - Bubble UI 구현
    
    Resolves: #이슈번호

---
+++ src/shared/components/Bubble.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import type { CalendarMarker } from "../types/post.type";
+
+const Bubble = ({ markers }: { markers: CalendarMarker[] }) => {
+  const titleMarker = markers[0];
+  const isEmoji = titleMarker.markerType === "EMOJI";
+
+  return (
+    <Container $isEmoji={isEmoji}>
+      {isEmoji ? titleMarker.markerEmoji : <Image src={"titleMarker.markerImageUrl as string"} />}
+    </Container>
+  );
+};
+
+export default Bubble;
+
+const Container = styled.div<{ $isEmoji: boolean }>`
+  position: absolute;
+  top: -10px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 40%;
+  height: 40%;
+  border-radius: 50%;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: ${({ $isEmoji }) => ($isEmoji ? "clamp(14px, 3vw, 30px)" : "0")};
+  line-height: 1;
+
+  overflow: hidden;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.12);
+`;
+
+const Image = styled.img`
+  position: absolute;
+  inset: 0;
+
+  width: 100%;
+  height: 100%;
+
+  object-fit: cover;
+`;
