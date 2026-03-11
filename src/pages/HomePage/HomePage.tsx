9d2762f1b7ebb31e00bf73f9033f30785fcdb912
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-11 22:48:35 +0900

    ✨ feat(fe): HomePage 생성 및 login 테스트 버튼 구현
    본문
    - HomePage 생성 및 login 테스트 버튼 구현
    
    Resolves: #이슈번호

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,23 @@
+const HomePage = () => {
+  const handleKakaoLogin = () => {
+    const clientId = import.meta.env.VITE_KAKAO_OAUTH_KEY;
+    const redirectUri = import.meta.env.VITE_REDIRECTION_URL;
+
+    const kakaoUrl =
+      `https://kauth.kakao.com/oauth/authorize` +
+      `?client_id=${clientId}` +
+      `&redirect_uri=${redirectUri}` +
+      `&response_type=code` +
+      `&state=kakao`;
+
+    window.location.href = kakaoUrl;
+  };
+  return (
+    <div>
+      HomePage
+      <button onClick={handleKakaoLogin}>login</button>
+    </div>
+  );
+};
+
+export default HomePage;
