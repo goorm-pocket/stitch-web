9b04c129fc093a18b142b079f4eaf4171b9cd3d9
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-12 00:39:11 +0900

    ✨ feat(fe): logout, fetchMe 기능 구현
    본문
    - logout, fetchMe 기능 구현
    
    Resolves: #이슈번호

--- src/shared/api/auth.ts
+++ src/shared/api/auth.ts
@@ -1,16 +1,39 @@
 import { apiClient } from "./axios";
 
+interface OauthLoginRes {
+  userId: string;
+  status: string;
+  needProfileSetup: boolean; // 프로필 초기 설정 필요 여부
+}
+
 export async function oauthLogin({
   provider,
   code,
 }: {
-  provider: string | null;
-  code: string | null;
-}) {
-  if (!code || !provider) return;
-  const res = await apiClient.post(`/api/v1/auth/${provider}/login`, {
+  provider: string;
+  code: string;
+}): Promise<OauthLoginRes> {
+  const res = await apiClient.post<OauthLoginRes>(`/api/v1/auth/${provider}/login`, {
     authorizationCode: code,
     redirectUri: import.meta.env.VITE_REDIRECTION_URL,
   });
+  return res.data;
+}
+
+export async function logout() {
+  const res = await apiClient.post("/api/v1/auth/logout");
+  return res.data;
+}
+
+interface fetchMeRes {
+  userId: string;
+  nickname: string;
+  systemRole: "USER" | "ADMIN" | "GUEST";
+  profileUrl: string;
+  isAgreed: boolean;
+}
+
+export async function fetchMe(): Promise<fetchMeRes> {
+  const res = await apiClient.get<fetchMeRes>("/api/v1/auth/me");
   return res.data;
 }
