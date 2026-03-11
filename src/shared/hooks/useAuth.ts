37a6f8122d192d8db57ae70002da92e0718a92e5
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-12 00:48:38 +0900

    ✨ feat(fe): logout, fetchMe tanstack 처리
    본문
    - logout mutation 처리
    - fetchMe query처리
    
    Resolves: #이슈번호

--- src/shared/hooks/useAuth.ts
+++ src/shared/hooks/useAuth.ts
@@ -1,8 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
-import { oauthLogin } from "../api/auth";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { fetchMe, logout, oauthLogin } from "../api/auth";
 
 export function useOauthLoginMutation() {
   return useMutation({
     mutationFn: oauthLogin,
+  });
+}
+
+export function useLogoutMutation() {
+  return useMutation({
+    mutationFn: logout,
+  });
+}
+
+export function useFetchMeQuery() {
+  return useQuery({
+    queryKey: ["me"],
+    queryFn: fetchMe,
   });
 }
