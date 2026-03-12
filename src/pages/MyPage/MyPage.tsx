e3d0940840105cd73c62fa401df21d7c7a5530ed
YJiiiin <jyjin9503@naver.com>
2026-03-12 13:25:32 +0900

    ✨ feat(fe): setting button 구현
    본문 (무엇을, 왜 변경했는지 상세 설명)
     - setting-icon.svg 추가
     - profile 우측 상단 setting button 구현
     - 버튼 클릭 시 페이지 이동
    
    Resolves: #이슈번호

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,10 +1,19 @@
 import styled from "styled-components";
+import SettingsIcon from "../../assets/setting-icon.svg";
+import { useNavigate } from "react-router";
 import { ProfileStitchedBox } from "../../shared/ui/StitchedBox";
 
 const MyPage = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
-      <ProfileContainer></ProfileContainer>
+      <ProfileContainer>
+        <SettingButton onClick={() => navigate("/setting")}>
+          <SettingIcon as={SettingsIcon} />
+          Settings
+        </SettingButton>
+      </ProfileContainer>
     </Container>
   );
 };
@@ -19,12 +28,39 @@ const Container = styled.main`
 `;
 
 const ProfileContainer = styled(ProfileStitchedBox)`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
   width: 100%;
-  height: 125px;
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
-  padding: 20px;
+  height: 200px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
+  padding: 30px;
   margin-bottom: 32px;
+`;
+
+const SettingButton = styled.button`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  background: ${({ theme }) => theme.colors.sub};
+  border: none;
+  border-radius: 8px;
+
+  color: white;
+  font-size: 12px;
+
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.2s;
+`;
+
+const SettingIcon = styled.svg`
+  color: currentColor;
+  margin-right: 5px;
 `;
