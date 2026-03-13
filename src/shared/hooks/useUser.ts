334de3eb8447a64182131a6b180754aab109aa4f
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-13 16:02:25 +0900

    ✨ feat(fe): usePatchNotificationSettingsMutation 구현
    본문 (무엇을, 왜 변경했는지 상세 설명)
    - patchNotificationSettings mutation 처리
    Resolves: #이슈번호

--- src/shared/hooks/useUser.ts
+++ src/shared/hooks/useUser.ts
@@ -1,10 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
-import { getNotificationSettings, getPrivacySettings } from "../api/user";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  getNotificationSettings,
+  getPrivacySettings,
+  patchNotificationSettings,
+} from "../api/user";
 
 export function useGetNotificationSettingsQuery() {
   return useQuery({
     queryKey: ["notifications-settings"],
     queryFn: getNotificationSettings,
+  });
+}
+
+export function usePatchNotificationSettingsMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: patchNotificationSettings,
+    onSuccess: (data) => {
+      queryClient.setQueryData(["notifications-settings"], data);
+    },
   });
 }
 
