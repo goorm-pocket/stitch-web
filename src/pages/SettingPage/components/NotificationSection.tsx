c565afc0d6094c84c33491fc4c8d4b5f3c0461fb
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-13 14:34:53 +0900

    ✨ feat(fe): settingPage 컨포넌트로 분할
    본문 (무엇을, 왜 변경했는지 상세 설명)
    - NotificationSection, PrivacySection, ActionSection 컨포넌트로 분할
    
    Resolves: #이슈번호

---
+++ src/pages/SettingPage/components/NotificationSection.tsx
@@ -0,0 +1,64 @@
+import { useGetNotificationSettingsQuery } from "@/shared/hooks/useAuth";
+import {
+  SectionHeader,
+  SectionItem,
+  SectionLeft,
+  SectionList,
+  SectionToggle,
+  SettingsSection,
+} from "../setting.styled";
+import type { NotificationSettings } from "@/shared/types/user.type";
+import type { ReactNode } from "react";
+
+import NotificationIcon from "@/assets/settings/notification-icon.svg";
+import FriendRequestIcon from "@/assets/settings/friend-request-icon.svg";
+import FriendAcceptIcon from "@/assets/settings/friend-accept-icon.svg";
+import CommentIcon from "@/assets/settings/comment-icon.svg";
+import MentionIcon from "@/assets/settings/mention-icon.svg";
+import LikeIcon from "@/assets/settings/like-icon.svg";
+import RecapIcon from "@/assets/settings/recap-icon.svg";
+
+const notificationItems: {
+  key: Exclude<keyof NotificationSettings, "pushEnabled">;
+  label: string;
+  icon: ReactNode;
+}[] = [
+  { key: "friendRequest", label: "Friend Request", icon: <FriendRequestIcon /> },
+  { key: "friendAccepted", label: "Friend Acceptance", icon: <FriendAcceptIcon /> },
+  { key: "comment", label: "Comment", icon: <CommentIcon /> },
+  { key: "mention", label: "Mention", icon: <MentionIcon /> },
+  { key: "postLike", label: "Post Like", icon: <LikeIcon /> },
+  { key: "weeklyRecap", label: "Weekly Recap", icon: <RecapIcon /> },
+];
+
+const NotificationSection = () => {
+  // 알람 설정 데이터
+  const { data: notificationStatesData } = useGetNotificationSettingsQuery();
+
+  const notificationStates = notificationStatesData?.notificationSettings;
+  const notificationEnabled = notificationStates?.pushEnabled ?? false;
+  return (
+    <SettingsSection>
+      <SectionHeader>
+        <NotificationIcon />
+        <div>Notification Settings</div>
+        <SectionToggle checked={notificationEnabled} />
+      </SectionHeader>
+
+      <SectionList $active={notificationEnabled}>
+        {notificationItems.map((item) => (
+          <SectionItem key={item.key}>
+            <SectionLeft>
+              {item.icon}
+              <div>{item.label}</div>
+            </SectionLeft>
+
+            <SectionToggle disabled={!notificationEnabled} />
+          </SectionItem>
+        ))}
+      </SectionList>
+    </SettingsSection>
+  );
+};
+
+export default NotificationSection;
---
+++ src/pages/SettingPage/components/PrivacySection.tsx
@@ -0,0 +1,57 @@
+import PrivacyIcon from "@/assets/settings/privacy-icon.svg";
+import NameIcon from "@/assets/settings/name-icon.svg";
+import BirthIcon from "@/assets/settings/birthday-icon.svg";
+import AgeIcon from "@/assets/settings/age-icon.svg";
+import type { PrivacySettings } from "@/shared/types/user.type";
+import type { ReactNode } from "react";
+import { useGetPrivacySettingsQuery } from "@/shared/hooks/useAuth";
+import {
+  SectionHeader,
+  SectionItem,
+  SectionLeft,
+  SectionList,
+  SectionToggle,
+  SettingsSection,
+} from "../setting.styled";
+
+const privacyItems: {
+  key: Exclude<keyof PrivacySettings, "isPublic">;
+  label: string;
+  icon: ReactNode;
+}[] = [
+  { key: "namePublic", label: "Real Name", icon: <NameIcon /> },
+  { key: "birthPublic", label: "Birth Date", icon: <BirthIcon /> },
+  { key: "agePublic", label: "Age Visibility", icon: <AgeIcon /> },
+];
+
+const PrivacySection = () => {
+  // 개인 정보 설정 데이터
+  const { data: privacyStatesData } = useGetPrivacySettingsQuery();
+
+  const privacyStates = privacyStatesData?.privacySettings;
+  const privacyEnabled = privacyStates?.isPublic ?? false;
+  return (
+    <SettingsSection>
+      <SectionHeader>
+        <PrivacyIcon />
+        <div>Privacy Settings</div>
+        <SectionToggle checked={privacyEnabled} />
+      </SectionHeader>
+
+      <SectionList $active={privacyEnabled}>
+        {privacyItems.map((item) => (
+          <SectionItem key={item.key}>
+            <SectionLeft>
+              {item.icon}
+              <div>{item.label}</div>
+            </SectionLeft>
+
+            <SectionToggle disabled={!privacyEnabled} />
+          </SectionItem>
+        ))}
+      </SectionList>
+    </SettingsSection>
+  );
+};
+
+export default PrivacySection;
