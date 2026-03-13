9ab6e71739d6fd44cdf2940a60ea2e8cc4177f6f
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-13 16:14:13 +0900

    ✨ feat(fe): usePatchPrivacySettingsMutation 구현
    본문 (무엇을, 왜 변경했는지 상세 설명)
    - patchPrivacySettings mutation 처리
    
    Resolves: #이슈번호

--- src/shared/hooks/useUser.ts
+++ src/shared/hooks/useUser.ts
@@ -3,6 +3,7 @@ import {
   getNotificationSettings,
   getPrivacySettings,
   patchNotificationSettings,
+  patchPrivacySettings,
 } from "../api/user";
 
 export function useGetNotificationSettingsQuery() {
@@ -27,5 +28,16 @@ export function useGetPrivacySettingsQuery() {
   return useQuery({
     queryKey: ["privacy-settings"],
     queryFn: getPrivacySettings,
+  });
+}
+
+export function usePatchPrivacySettingsMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: patchPrivacySettings,
+    onSuccess: (data) => {
+      queryClient.setQueryData(["privacy-settings"], data);
+    },
   });
 }
