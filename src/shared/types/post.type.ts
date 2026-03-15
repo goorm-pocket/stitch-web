0fb93ec696dfe1f0834a24a9db543f724093fb61
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-15 16:49:01 +0900

    ✨ feat(fe): post type 정의
    본문 (무엇을, 왜 변경했는지 상세 설명)
    
    Resolves: #이슈번호

--- src/shared/types/post.type.ts
+++ src/shared/types/post.type.ts
@@ -15,3 +15,41 @@ export interface Calendar {
   markers: CalendarMarker[];
   hasMultiplePosts: boolean; // postCount가 2 이상인지
 }
+
+export interface Post {
+  post_id: string;
+
+  author: PostAuthor;
+
+  content: string | null;
+  visibility: PostVisibility;
+
+  like_count: number;
+  liked_by_me: boolean;
+
+  created_at: string;
+  updated_at: string;
+
+  editable_until: string;
+  is_editable: boolean;
+
+  marker_type: Marker;
+  marker_emoji: string | null;
+  marker_image_url: string | null;
+
+  images: PostImage[];
+}
+
+export interface PostAuthor {
+  user_id: string;
+  nickname: string;
+  profile_image_url: string | null;
+}
+
+export interface PostImage {
+  image_url: string;
+}
+
+export type PostVisibility = "PUBLIC" | "FRIENDS" | "PRIVATE";
+
+export type Marker = "EMOJI" | "IMAGE";
