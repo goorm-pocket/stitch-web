14043088259c316727f6ea93620bcd8a7a65727d
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-19 16:18:06 +0900

    ✨ feat(fe): getCalendar, useGetCalendarQuery 기능 구현
    본문 (무엇을, 왜 변경했는지 상세 설명)
    - getCalendar, useGetCalendarQuery 기능 구현
    
    Resolves: #이슈번호

--- src/shared/hooks/usePost.ts
+++ src/shared/hooks/usePost.ts
@@ -1,10 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import { getPostById } from "../api/post";
+import { getCalendar, getPostById } from "../api/post";
 
 export function useGetPostByIdQuery({ postId }: { postId: string }) {
   return useQuery({
     queryKey: ["post", postId],
     queryFn: () => getPostById({ postId }),
     enabled: !!postId,
+  });
+}
+
+export function useGetCalendarQuery({ year, month }: { year: number; month: number }) {
+  return useQuery({
+    queryKey: ["calendar", "me"],
+    queryFn: () => getCalendar({ year, month }),
+    enabled: !!year && !!month,
   });
 }
