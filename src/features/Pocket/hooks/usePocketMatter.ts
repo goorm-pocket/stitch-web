5b33b4830d340371acb34769c02a712ee71f38d1
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-21 19:46:42 +0900

    ✨ feat(fe): usePocketMatter 구현
    본문 (무엇을, 왜 변경했는지 상세 설명)
    - usePocketMatter 구현
    
    Resolves: #이슈번호

---
+++ src/features/Pocket/hooks/usePocketMatter.ts
@@ -0,0 +1,111 @@
+import type { PocketBubbleType } from "@/shared/types/post.type";
+import { useEffect, useRef, useState } from "react";
+import { createPocketBodies, type BodyMap } from "../utils/createPocketBodies";
+import Matter from "matter-js";
+import { createPocketWalls } from "../utils/createPocketWalls";
+
+export interface PositionMap {
+  [postId: string]: {
+    x: number;
+    y: number;
+    angle: number;
+  };
+}
+
+interface UsePocketMatterParams {
+  sceneRef: React.RefObject<HTMLDivElement | null>;
+  items: PocketBubbleType[];
+  width: number;
+  height: number;
+  itemSize: number;
+  wallThickness: number;
+}
+
+export function usePocketMatter({
+  sceneRef,
+  items,
+  width,
+  height,
+  itemSize,
+  wallThickness,
+}: UsePocketMatterParams) {
+  const [positions, setPositions] = useState<PositionMap>({}); // bubble들 위치 저장
+  const runnerRef = useRef<Matter.Runner | null>(null); // 공들 계속 움직히게 함
+  const animationRef = useRef<number | null>(null); // 화면 업데이트 타이머
+  const bodyMapRef = useRef<BodyMap>({}); // bubble들의 Ref
+
+  useEffect(() => {
+    if (!sceneRef.current || width === 0 || height === 0 || items.length === 0) return;
+    bodyMapRef.current = {};
+
+    const engine = Matter.Engine.create();
+    engine.gravity.y = 3;
+
+    // pocket 생성
+    const world = engine.world;
+
+    // 벽 생성
+    const walls = createPocketWalls({ width, height, wallThickness });
+
+    Matter.World.add(world, walls); // world에 벽 넣기
+
+    const bodies = createPocketBodies({
+      items,
+      width,
+      height,
+      itemSize,
+      bodyMapRef,
+    });
+
+    Matter.World.add(world, bodies);
+
+    // 엔진 안에서 마우스 이벤트를 추적
+    const mouse = Matter.Mouse.create(sceneRef.current);
+
+    // 마우스와 물리 엔진을 끈으로 연결
+    const mouseConstraint = Matter.MouseConstraint.create(engine, {
+      mouse,
+      constraint: {
+        stiffness: 0.05, // 얼마나 잘 붙어있는지
+        render: { visible: false }, // 끈 보이게 할지
+      },
+    });
+    Matter.World.add(world, mouseConstraint);
+
+    // 물리 엔진을 계속 실행시키는 루프 생성
+    const runner = Matter.Runner.create();
+    runnerRef.current = runner;
+    Matter.Runner.run(runner, engine);
+    const updatePositions = () => {
+      const next: PositionMap = {};
+
+      items.forEach((item) => {
+        const body = bodyMapRef.current[item.postId];
+        if (!body) return;
+
+        next[item.postId] = {
+          x: body.position.x,
+          y: body.position.y,
+          angle: body.angle,
+        };
+      });
+
+      setPositions(next);
+      animationRef.current = requestAnimationFrame(updatePositions);
+    };
+
+    updatePositions();
+
+    return () => {
+      if (animationRef.current) {
+        cancelAnimationFrame(animationRef.current);
+      }
+
+      Matter.Runner.stop(runner);
+      Matter.World.clear(world, false);
+      Matter.Engine.clear(engine);
+    };
+  }, [sceneRef, items, width, height, itemSize, wallThickness]);
+
+  return positions;
+}
