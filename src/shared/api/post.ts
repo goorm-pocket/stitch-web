2d360c4c1d11b4ed75964a3655c42e7f74fbc156
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-23 20:36:11 +0900

    ✨ feat(fe): deletePostLike 기능 구현
    본문 (무엇을, 왜 변경했는지 상세 설명)
    - deletePostLike 기능 구현
    - mutation 처리
    
    Resolves: #이슈번호

--- src/shared/api/post.ts
+++ src/shared/api/post.ts
@@ -48,3 +48,8 @@ export async function createPostLike({ postId }: { postId: string }) {
   const res = await apiClient.post(`/api/v1/posts/${postId}/likes`);
   return res.data.data;
 }
+
+export async function deletePostLike({ postId }: { postId: string }) {
+  const res = await apiClient.delete(`/api/v1/posts/${postId}/likes`);
+  return res.data.data;
+}
--- src/shared/hooks/usePost.ts
+++ src/shared/hooks/usePost.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createPost, createPostLike } from "../api/post";
+import { createPost, createPostLike, deletePostLike } from "../api/post";
 import { useQuery } from "@tanstack/react-query";
 import { getCalendar, getPostById } from "../api/post";
 
@@ -34,6 +34,16 @@ export function useCreatePostLikeMutation({ postId }: { postId: string }) {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: createPostLike,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["post", postId] });
+    },
+  });
+}
+
+export function useDeletePostLikeMutation({ postId }: { postId: string }) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: deletePostLike,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["post", postId] });
     },
