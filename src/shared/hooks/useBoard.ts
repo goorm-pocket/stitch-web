9e3b0c4da47fe1950b25ccfe7c2c4eff6c66f7b2
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-23 21:30:33 +0900

    ✨ feat(fe): readBoardPost 기능 구현
    본문 (무엇을, 왜 변경했는지 상세 설명)
    - readBoardPost 기능 구현
    - mutation 처리
    
    Resolves: #이슈번호

--- src/shared/hooks/useBoard.ts
+++ src/shared/hooks/useBoard.ts
@@ -1,10 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
-import { getBoard } from "../api/board";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getBoard, readBoardPost } from "../api/board";
 
 export function useGetBoardQuery(platform: "WEB" | "APP") {
   return useQuery({
     queryKey: ["board", platform],
     queryFn: () => getBoard({ platform }),
     enabled: !!platform,
+  });
+}
+
+export function useReadBoardPostMutation() {
+  return useMutation({
+    mutationFn: readBoardPost,
   });
 }
