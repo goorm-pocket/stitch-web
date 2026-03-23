450a5f27bdeef07edbd3a8f610cb542f3ec7df5a
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-23 21:52:04 +0900

    ✨ feat(fe): read board post 시 board를 다시 받아오는 기능 구현
    본문 (무엇을, 왜 변경했는지 상세 설명)
    - read board post 시 board를 다시 받아오는 기능 구현
    
    Resolves: #이슈번호

--- src/shared/hooks/useBoard.ts
+++ src/shared/hooks/useBoard.ts
@@ -1,16 +1,20 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBoard, readBoardPost } from "../api/board";
 
 export function useGetBoardQuery(platform: "WEB" | "APP") {
   return useQuery({
-    queryKey: ["board", platform],
+    queryKey: ["board"],
     queryFn: () => getBoard({ platform }),
     enabled: !!platform,
   });
 }
 
 export function useReadBoardPostMutation() {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: readBoardPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["board"] });
+    },
   });
 }
